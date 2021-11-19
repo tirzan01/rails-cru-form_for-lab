7a5a7a240a112646ff4f42cea2bc597091eaf4af
Thomas Montalto <thomasmonta98@gmail.com>
2021-11-19 17:13:45 +1100

    done with assignment

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,34 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new
+    @song.name = params[:song][:name]
+    @song.artist_id = params[:song][:artist_id]
+    @song.genre_id = params[:song][:genre_id]
+    @song.save(params.require(:song).permit(:name, :artist_id, :genre_id))
+    redirect_to(@song)
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
+    redirect_to(@song)
+  end
+
+end
